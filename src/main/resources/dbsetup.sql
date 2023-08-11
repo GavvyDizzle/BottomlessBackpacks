eca4c22b3b0bcdc12b099a9ce1482d72c41412ba
gjdim <gjdimpter@gmail.com>
2023-08-10 22:20:50 -0400

    Use MEDIUMBLOB instead of BLOB

--- src/main/resources/dbsetup.sql
+++ src/main/resources/dbsetup.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS backpacks
     uuid BINARY(16) NOT NULL,
     pages INT DEFAULT 1,
     savePage BOOLEAN DEFAULT 0,
-    items BLOB,
+    items MEDIUMBLOB,
     PRIMARY KEY (uuid)
 );
